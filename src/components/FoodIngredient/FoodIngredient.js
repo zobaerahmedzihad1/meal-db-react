da4427e7c21afd59e0a7e20fd8675e8bae47993e
zobaerahmedzihad1 <zobaerahmedzihad02@gmail.com>
2022-03-30 21:44:53 +0600

    daynamic route and load data by categoray

--- src/components/FoodIngredient/FoodIngredient.js
+++ src/components/FoodIngredient/FoodIngredient.js
@@ -5,13 +5,16 @@ const FoodIngredient = () => {
   const { strCategory } = useParams();
   const [category, setCategory] = useState([]);
   useEffect(() => {
-       const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=chicken`
-       fetch()
+    const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${strCategory}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setCategory(data.meals[0]));
   }, [strCategory]);
 
   return (
     <div>
-      <h3>Food Ingredient</h3>
+      <h2>{strCategory} Foods</h2>
+      <h5>{category.strMeal}</h5>
     </div>
   );
 };
